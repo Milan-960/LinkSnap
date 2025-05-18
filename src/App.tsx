941e5e1191762861c63615904be3ac508c3ae285
Milan_sacahni <milan.sachani.ms@gmail.com>
2025-05-18 12:55:33 +0200

    eslint error updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { Routes, Route } from "react-router-dom";
-import { AnimatePresence } from "framer-motion";
-import Home from "./pages/Home";
-import Shortened from "./pages/Shortened";
-import Redirect from "./pages/Redirect";
-import NotFound from "./pages/NotFound";
-import About from "./pages/About";
-import History from "./pages/History";
-import Layout from "./layout/Layout";
+import { Routes, Route } from 'react-router-dom'
+import { AnimatePresence } from 'framer-motion'
+import Home from './pages/Home'
+import Shortened from './pages/Shortened'
+import Redirect from './pages/Redirect'
+import NotFound from './pages/NotFound'
+import About from './pages/About'
+import History from './pages/History'
+import Layout from './layout/Layout'
 
 export default function App() {
   return (
@@ -16,14 +16,15 @@ export default function App() {
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
           <Route path="/shortened/:code" element={<Shortened />} />
+          <Route path="/go/:code" element={<Redirect />} />
           <Route path="/about" element={<About />} />
           <Route path="/link-history" element={<History />} />
           <Route path="*" element={<NotFound />} />
         </Route>
 
         {/* Routes without layout */}
-        <Route path="/go/:code" element={<Redirect />} />
+        {/* <Route path="/go/:code" element={<Redirect />} /> */}
       </Routes>
     </AnimatePresence>
-  );
+  )
 }
